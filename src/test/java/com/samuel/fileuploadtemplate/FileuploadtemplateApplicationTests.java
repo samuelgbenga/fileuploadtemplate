c23b605ab326423704a09264269d481efb1e466a
samuelgbenga <samuelgbenga972@gmail.com>
2024-12-11 11:36:19 +0100

    new commit

---
+++ src/test/java/com/samuel/fileuploadtemplate/FileuploadtemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.fileuploadtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadtemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
